2b5dcdd4f7ad26eefd4095030ca7e633adf169c9
AnTran <vietanisme@gmail.com>
2018-10-14 16:19:11 +0700

    Paper finished, working on Detail

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react';
-import IndexPage from './Layouts/IndexPage';
+import React, { Component } from "react";
+import Review from "./Components/Review";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-        <IndexPage/>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Review
+                    reviewList={[
+                        { user: "An Tran", content: "Cool" },
+                        { user: "An Tran", content: "Cool" }
+                    ]}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/Components/Paper/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+const styles = theme => ({
+    root: {
+        ...theme.mixins.gutters(),
+        paddingTop: theme.spacing.unit * 2,
+        paddingBottom: theme.spacing.unit * 2,
+    },
+});
+
+function PaperSheet(props) {
+    const { className, classes, children, style} = props;
+    return (
+        <div className={className} style={style}>
+            <Paper className={classes.root} elevation={1}>
+                {children}
+            </Paper>
+        </div>
+    );
+}
+
+export default withStyles(styles)(PaperSheet);
